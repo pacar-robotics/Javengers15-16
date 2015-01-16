378362dc68b16bbeab49d0500b43690ed932b095
KoolKenK <koolkenk@gmail.com>
2015-01-16 13:25:25 -0500

    Added Comments

--- DisplayIRBeacon.c
+++ DisplayIRBeacon.c
@@ -1,7 +1,13 @@
+
 #pragma config(Sensor, S2,     irseeker,       sensorHiTechnicIRSeeker1200)
 
-#include "hitechnic-irseeker-v2.h"
+/*
+			Display IR Beacon Values
+																*/
 
+#include "hitechnic-irseeker-v2.h"	//for ir seeker
+
+//ir wave values
 #define IR600 DSP_600
 #define IR1200 DSP_1200
 
@@ -12,7 +18,7 @@ tHTIRS2 irSeeker;
 task main()
 {
 	initSensor(&irSeeker, S2);
-	irSeeker.mode = IR600;
+	irSeeker.mode = IR600;	//mode set to 600
 
 	displayIRBeaconValues();
 }
@@ -24,20 +30,19 @@ void displayIRBeaconValues()
 	{
 		eraseDisplay();
 		readSensor(&irSeeker);
-		displayTextLine(1, "Sensors");
-		displayTextLine(2, "Dir-%d EnhDir-%d",irSeeker.acDirection,irSeeker.enhDirection);
-		displayTextLine(3, "1-f%d 2-%d 3-%d",
-		irSeeker.acValues[0],
-		irSeeker.acValues[1],
-		irSeeker.acValues[2],
-		);
 
+		displayTextLine(1, "Sensors");
+
+		//displays directional value
+		displayTextLine(2, "Dir-%d EnhDir-%d",irSeeker.acDirection,irSeeker.enhDirection);
+		displayTextLine(3, "1-f%d 2-%d 3-%d", irSeeker.acValues[0], irSeeker.acValues[1], irSeeker.acValues[2]);
 		displayTextLine(4, "4-%d 5-%d",
 		irSeeker.acValues[3],
 		irSeeker.acValues[4]);
 
 		displayTextLine(5, "Strength:%d", irSeeker.enhStrength);
 
+		//tells what position center field goal is in
 		if(irSeeker.enhStrength>9)
 		{
 			Position=3;
@@ -53,5 +58,5 @@ void displayIRBeaconValues()
 
 		displayTextLine(6,"Position: %d", Position);
 		wait1Msec(500);
-	}
-}
+	} //while
+} //end of function
