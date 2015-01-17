ac00833ff3f635f5a8e94ea1e7e0ad7a20d0372e
rachaelchacko <rachaelchacko@yahoo.com>
2015-01-16 22:53:25 -0500

    Now reads from choices.c
    Reads input from choices

--- DisplayIRBeacon.c
+++ DisplayIRBeacon.c
@@ -1,4 +1,3 @@
-
 #pragma config(Sensor, S2,     irseeker,       sensorHiTechnicIRSeeker1200)
 
 /*
@@ -10,6 +9,10 @@
 //ir wave values
 #define IR600 DSP_600
 #define IR1200 DSP_1200
+
+//file used in Choice.c
+#define DATA_FILE_NAME "choices.txt"
+
 
 void displayIRBeaconValues();
 
@@ -26,7 +29,35 @@ task main()
 void displayIRBeaconValues()
 {
 	int Position;
-	while(1)
+	//tHTIRS2DSPMode irFrequency;
+	TFileIOResult nIoResult;
+	TFileHandle myFileHandle;
+	short myFileSize = 10;
+	short irChoice;
+
+	OpenRead(myFileHandle, nIoResult, DATA_FILE_NAME, myFileSize);
+
+	if(nIoResult) // Error in opening file for read
+	{
+		// NEED A BACK-UP PLAN
+		playTone(5000, 5);
+	  stopAllTasks();
+	}
+
+	//reads choices
+	ReadShort(myFileHandle, nIoResult, irChoice);
+
+	//sets values depending on choices
+	if(irChoice == 1200)
+	{
+		irSeeker.mode = IR1200;
+	}
+	else
+	{
+		irSeeker.mode = IR600;
+	}
+
+	while(true)
 	{
 		eraseDisplay();
 		readSensor(&irSeeker);
@@ -42,14 +73,15 @@ void displayIRBeaconValues()
 
 		displayTextLine(5, "Strength:%d", irSeeker.enhStrength);
 
+
 		//tells what position center field goal is in
 		if(irSeeker.enhStrength>9)
 		{
-			Position=3;
+			Position = 3;
 		}
-		else if((irSeeker.enhStrength>5)&(irSeeker.acValues[1]>5))
+		else if((irSeeker.enhStrength > 5) & (irSeeker.acValues[1] > 5))
 		{
-			Position=2;
+			Position = 2;
 		}
 		else
 		{
@@ -58,5 +90,5 @@ void displayIRBeaconValues()
 
 		displayTextLine(6,"Position: %d", Position);
 		wait1Msec(500);
-	} //while
-} //end of function
+	} //while(true)
+}
